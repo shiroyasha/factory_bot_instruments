41aedd6cee757e98bccb2764f2f537fc85a63c75
Igor Šarčević <igor@renderedtext.com>
2017-05-03 16:08:29 +0200

    Test for benchmark_all limiters

--- spec/factory_girl_benchmark_spec.rb
+++ spec/factory_girl_benchmark_spec.rb
@@ -33,6 +33,47 @@ RSpec.describe FactoryGirlBenchmark do
     end
   end
 
+  describe ".benchmark_all" do
+    it "keeps the db clean" do
+      expect { FactoryGirlBenchmark.benchmark_all }.to_not change { User.count }
+    end
+
+    it "benchmarks all factories" do
+      benchmarked_factories = FactoryGirlBenchmark.benchmark_all.map(&:factory)
+
+      expect(benchmarked_factories).to include(:user)
+      expect(benchmarked_factories).to include(:article)
+    end
+
+    it "benchmarks by create, build, and build_stubbed by default" do
+      benchmarked_methods = FactoryGirlBenchmark.benchmark_all.map(&:method)
+
+      expect(benchmarked_methods).to include(:create)
+      expect(benchmarked_methods).to include(:build_stubbed)
+      expect(benchmarked_methods).to include(:build)
+    end
+
+    describe "limiting factory girl methods" do
+      it "runs only passed factory girl methods" do
+        benchmarked_methods = FactoryGirlBenchmark.benchmark_all(:methods => [:create, :build]).map(&:method)
+
+        expect(benchmarked_methods).to include(:create)
+        expect(benchmarked_methods).to include(:build)
+
+        expect(benchmarked_methods).to_not include(:build_stubbed)
+      end
+    end
+
+    describe "skipping factories" do
+      it "skipps passed factories" do
+        benchmarked_factories = FactoryGirlBenchmark.benchmark_all(:except => [:article]).map(&:factory)
+
+        expect(benchmarked_factories).to include(:user)
+        expect(benchmarked_factories).to_not include(:article)
+      end
+    end
+  end
+
   describe ".benchmark" do
     it "keeps the db clean" do
       expect { FactoryGirlBenchmark.benchmark(:user) }.to_not change { User.count }
